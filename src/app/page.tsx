7fe7b476e33ab2b7e7a58c8d5d48d788a185ef18
Guilherme Souza <gui.edu0380@gmail.com>
2025-01-27 21:33:49 -0300

    feat adicionando tela inicial da página

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,75 @@
+'use client'
+
 import Image from "next/image";
+import { useEffect } from "react";
+import imagemTeste from '@/icons/imagemTeste.jpg'
+import { Kanit } from 'next/font/google'
+import styles from '@/css/portfolioImageStyle.module.css'
+import githubIcon from '@/icons/github.svg'
+import whatsappIcon from '@/icons/whatsapp.svg'
+import linkedinIcon from '@/icons/linkedin.svg'
+import ScrollArrow from "@/components/Scrollicon";
+
+const kanit = Kanit({ weight: '400' })
+const waveEmoji = <span role="img" aria-label="waveHand">👋</span>
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div id='body' className={`${kanit.className} h-full`}>
+      <button className="flex justify-center w-full" onClick={() => {
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      }}>Alterar Tema</button>
+
+      <div className="flex justify-center w-full my-40">
+        <div className="max-w-5xl">
+          <div className="text-center">
+            <h3 className="text-3xl">Olá! {waveEmoji}</h3>
+            <h2 className="text-6xl">Eu me chamo Guilherme Souza</h2>
+            <h3 className={"text-3xl text-hotyellow "}>Desenvolvedor Fullstack</h3>
+          </div>
+
+          <p className="text-xl mt-16">
+            Entusiasta em construir sistemas e criar soluções criativas sempre com foco no usuário final, encontro
+            motivação transformando boas ideias em realidade.
+          </p>
+
+          <div className="flex justify-between align-middle mt-8">
+            <button className="p-3 text-hotyellow border-2 border-solid border-hotyellow">Entre em contato comigo! </button>
+
+            <div id='socialMedias' className="flex gap-3 items-center">
+              <div id='linkedinIcon'>
+                <a href="https://www.linkedin.com/in/Guilhsouza" target="_blank">
+                  <Image alt="GitHub icon" src={linkedinIcon} width={40} className="rounded-lg" />
+                </a>
+              </div>
+              <div id='githubIcon'>
+                <a href="https://github.com/Guilhsouza" target="_blank">
+                  <Image alt="GitHub icon" src={githubIcon} width={40} className="rounded-lg" />
+                </a>
+              </div>
+              <div id='wppIcon'>
+                <a href="http://wa.me/5512991112772" target="_blank">
+                  <Image alt="GitHub icon" src={whatsappIcon} width={40} />
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+
+        <div className={styles.imageWrapper}>
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            alt="Minha foto"
+            src={imagemTeste}
+            fill
+            priority
+            className={`rounded-full border-hotyellow border-2 ${styles.imageShadow}`}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </div>
+      </div>
+
+      <div className="mt-4 flex justify-center">
+        <ScrollArrow />
+      </div>
     </div>
   );
 }
