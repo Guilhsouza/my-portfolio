8e63295d87b310521ceb82ff97d2f70809624183
Guilherme Souza <gui.edu0380@gmail.com>
2025-01-27 21:30:56 -0300

    feat adicionando cores padrão

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,8 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        hotyellow: "#FFD966",
+        goldyellow: "#bf9b30"
       },
     },
   },
