b257dc2aa71797bc64302474daf0d7645fba2993
Guilherme Souza <gui.edu0380@gmail.com>
2025-01-27 21:31:50 -0300

    feat adicionando icone de scroll

---
+++ src/components/Scrollicon.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import '@/css/scrollicon.scss'
+
+const ScrollArrow: React.FC = () => {
+    return (
+        <div>
+            <div className="scroll-arrow"></div>
+            <div className="scroll-arrow"></div>
+            <div className="scroll-arrow"></div>
+        </div>
+    )
+}
+
+export default ScrollArrow
