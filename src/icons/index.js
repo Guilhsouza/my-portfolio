5ee1af509ff4b522f8a3ea3f77023374cbb3924c
Guilherme Souza <gui.edu0380@gmail.com>
2025-02-05 16:31:05 -0300

    feat adicionando arquivo unico para importação dos icones svg

---
+++ src/icons/index.js
@@ -0,0 +1,27 @@
+import htmlIcon from './frontend/htmlicon.svg'
+import cssIcon from './frontend/cssicon.svg'
+import jsIcon from './frontend/js-icon.svg'
+import reactIcon from './frontend/reacticon.svg'
+import nextIcon from './frontend/nexticon.svg'
+import tailwindIcon from './frontend/tailwindicon.svg'
+
+import nodeIcon from './backend/nodeicon.svg'
+import expressIcon from './backend/expressicon.svg'
+import postgresIcon from './backend/postgreicon.svg'
+import mongoIcon from './backend/mongoicon.svg'
+import javaIcon from './backend/javaicon.svg'
+
+import dockerIcon from './tools/dockericon.svg'
+import gitIcon from './tools/giticon.svg'
+import gitHubIcon from './tools/githubicon.svg'
+import postmanIcon from './tools/postmanicon.svg'
+
+import githubDarkIcon from './socialMedias/github.svg'
+import wppIcon from './socialMedias/whatsapp.svg'
+import linkedinIcon from './socialMedias/whatsapp.svg'
+
+export {
+    jsIcon, htmlIcon, cssIcon, reactIcon, nextIcon, tailwindIcon,
+    nodeIcon, expressIcon, postgresIcon, mongoIcon, javaIcon, dockerIcon,
+    gitHubIcon, gitIcon, postmanIcon, githubDarkIcon, wppIcon, linkedinIcon
+}
