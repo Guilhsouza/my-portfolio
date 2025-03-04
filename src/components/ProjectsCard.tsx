672f87fb11d30dc2dc64a95110efec4a9791782a
Guilherme Souza <gui.edu0380@gmail.com>
2025-03-04 12:27:51 -0300

    feat adicionando imagens para o carousel do modal

--- src/components/ProjectsCard.tsx
+++ src/components/ProjectsCard.tsx
@@ -6,12 +6,14 @@ import ImageWithModal from "./ImageWithModal"
 interface ProjectCardBody {
     headerContent: string,
     bodyImage: string | StaticImageData,
+    image2: string | StaticImageData,
+    image3: string | StaticImageData,
     altImage: string,
     stacks: ReactNode[],
     description: string
 }
 
-const ProjectCard: React.FC<ProjectCardBody> = ({ headerContent, bodyImage, altImage, stacks, description }) => {
+const ProjectCard: React.FC<ProjectCardBody> = ({ headerContent, bodyImage, image2, image3, altImage, stacks, description }) => {
     return (
         <>
             <Card className="max-w-[370px] flex-col bg-gray-900 rounded-lg">
@@ -21,6 +23,8 @@ const ProjectCard: React.FC<ProjectCardBody> = ({ headerContent, bodyImage, altI
                 <CardBody id="imageFromProjects">
                     <ImageWithModal
                         bodyImage={bodyImage}
+                        image2={image2}
+                        image3={image3}
                         altImage={altImage}
                     />
                     <div className="flex gap-2 mt-4">
