920349ff0d6bb22bb0ffe798920f9b96efcfb27e
Guilherme Souza <gui.edu0380@gmail.com>
2025-03-06 21:13:07 -0300

    feat adicionando imagens para o modal de api de estoque online

--- src/images/index.js
+++ src/images/index.js
@@ -1,6 +1,10 @@
+import imagemTeste from './imagemTeste.jpg'
+
 import imperioEstoqueSofas from './imperioEstoqueSofas.png'
 import imperioSofasMenu from './imperioSofasMenu.png'
 import imperioEstoqueCamasEColchoes from './imperioEstoqueCamasEColchoes.png'
-import imagemTeste from './imagemTeste.jpg'
+import postmanApiEstoque from './postmanApiEstoque.png'
+import swagger1 from './swagger1.png'
+import swagger2 from './swagger2.png'
 
-export { imperioEstoqueSofas, imperioSofasMenu, imperioEstoqueCamasEColchoes, imagemTeste }
+export { imperioEstoqueSofas, imperioSofasMenu, imperioEstoqueCamasEColchoes, postmanApiEstoque, swagger1, swagger2, imagemTeste }
